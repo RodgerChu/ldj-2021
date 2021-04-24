ba69958cb362792a43ac6ff77c1582dc3e9225b0
v.gailich <v.gailich@corp.nekki.ru>
2021-04-24 20:12:52 +0300

    Добавлен источник света, прототип светлячка, экран смерти

--- Assets/Resources/Scripts/Player/Player.cs
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -6,4 +6,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private CharacterController _controller;
     [SerializeField] private Character _character;
+    [SerializeField] private PlayerLightController _lightController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        var gObject = collision.gameObject;
+        var glowworm = gObject.GetComponent<GlowwormCollectable>();
+        if (glowworm != null)
+        {
+            if (_lightController.AddPower())
+                Destroy(glowworm.gameObject);
+        }
+    }
 }
--- Assets/Resources/Scripts/PlayerLight/PlayerLightController.cs
+++ Assets/Resources/Scripts/PlayerLight/PlayerLightController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,35 +6,43 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class PlayerLightController : MonoBehaviour
 {
-    [SerializeField] private float _lightFadeTickTime = 0.5f;
-    [SerializeField] private float _lightFadeTickValue = 0.1f;
-    [SerializeField] private float _lightFadeDelayAfterLightPowerUp = 5f;
-
-    [Space]
-    [SerializeField] private float _minimalLightRadius = 0.5f;
-    [SerializeField] private float _maximumLightRadius = 3.5f;
+    [SerializeField] private LightSettings _lightSettings;
 
     [Space]
     [SerializeField] private Light2D _playerLight;
+
+    public Action OnLightFaded;
+
+    private float _maximumLightRadius => _lightSettings.MaximumLightRadius;
+    private float _minimalLightRadius => _lightSettings.MinimalLightRadius;
+    private float _lightFadeDelayAfterLightPowerUp => _lightSettings.LightFadeDelayAfterLightPowerUp;
+    private float _lightFadeTickValue => _lightSettings.LightFadeTickValue;
+    private float _lightFadeTickTime => _lightSettings.LightFadeTickTime;
 
     private Coroutine _fadeCoroutine;
     private float _delay = 0f;
 
     private void Start()
     {
+        _playerLight.pointLightOuterRadius = _lightSettings.MaximumLightRadius;
         StartFadeLight();
     }
 
-    public void AddPower(float power)
+    public bool AddPower()
     {
         var currPower = _playerLight.pointLightOuterRadius;
-        currPower += power;
+        var additionalPower = _maximumLightRadius / 10;
+        currPower += additionalPower;
 
         if (currPower > _maximumLightRadius)
-            currPower = _maximumLightRadius;
+        {
+            return false;
+        }
 
         _playerLight.pointLightOuterRadius = currPower;
         _delay = _lightFadeDelayAfterLightPowerUp;
+
+        return true;
     }
 
     public void StartFadeLight()
@@ -53,8 +62,14 @@ public class PlayerLightController : MonoBehaviour
         _fadeCoroutine = null;
     }
 
+    public void ResetLight()
+    {
+        _playerLight.pointLightOuterRadius = _lightSettings.MaximumLightRadius;
+    }
+
     private IEnumerator FadeCoroutine()
     {
+        var callbackFired = false;
         while(true)
         {
             if (_delay != 0)
@@ -64,7 +79,13 @@ public class PlayerLightController : MonoBehaviour
             }
 
             while (_playerLight.pointLightOuterRadius < _minimalLightRadius)
+            {
+                if (!callbackFired)
+                    OnLightFaded?.Invoke();
+                callbackFired = true;
+
                 yield return null;
+            }
 
             _playerLight.pointLightOuterRadius -= _lightFadeTickValue;
             yield return new WaitForSeconds(_lightFadeTickTime);
