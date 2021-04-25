627613b7befd4638200a6394e995e92638f3a639
Ivanonizm <bivanishe@gmail.com>
2021-04-25 18:33:50 +0300

    Базовые анимации перса
    Базовые анимации перса

--- Assets/Resources/Scripts/Common/CommonFlipper.cs
+++ Assets/Resources/Scripts/Common/CommonFlipper.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CommonFlipper : MonoBehaviour
 {
     [SerializeField] List<Transform> _transformToFlip;
-    [SerializeField] List<SpriteRenderer> _srToFlip;
+    [SerializeField] List<GameObject> _objectToFlip;
     public void SetFlip(float directionX)
     {
         if (_transformToFlip != null)
@@ -18,13 +18,18 @@ public class CommonFlipper : MonoBehaviour
             }
         }
 
-        if (_srToFlip != null)
+        if (_objectToFlip != null)
         {
-            foreach (var sr in _srToFlip)
+            foreach (var obj in _objectToFlip)
             {
-                bool flipX = sr.flipX;
-                flipX = (directionX < 0f) ? true : false;
-                sr.flipX = flipX;
+                if (directionX > 0)
+                {
+                    obj.transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+                else if (directionX < 0)
+                {
+                    obj.transform.rotation = Quaternion.Euler(0, 180, 0);
+                }
             }
         }
     }
